d46a5fedbfa7863cd23dd5377d342a90ea417a90
Chempari <mewtwobv@gmail.com>
2025-11-27 09:55:12 -0500

    Add unit test for Calculadora class

---
+++ src/test/java/com/ejemplo/CalculadoraTest.java
@@ -0,0 +1,13 @@
+package com.ejemplo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculadoraTest {
+    @Test
+    public void testSumar() {
+        Calculadora calc = new Calculadora();
+
+        assertEquals(5, calc.sumar(2, 3), "La suma de 2 + 3 debería ser 5");
+    }
+}
